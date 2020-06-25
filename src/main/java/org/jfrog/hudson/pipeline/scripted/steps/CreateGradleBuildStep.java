9da1d8cc7f3b11a441cbb396029d8db04cb017ba
Liron Shalom <52914174+liron-shalom@users.noreply.github.com>
2020-06-25 14:59:11 +0300

    Update CreateGradleBuildStep.java

--- src/main/java/org/jfrog/hudson/pipeline/scripted/steps/CreateGradleBuildStep.java
+++ src/main/java/org/jfrog/hudson/pipeline/scripted/steps/CreateGradleBuildStep.java
@@ -13,8 +13,8 @@ public class CreateGradleBuildStep extends AbstractStepImpl {
     }
 
     /**
-     * We doesn't use additional context fields in this step execution,
-     * so we extends SynchronousStepExecution directly and not ArtifactorySynchronousStepExecution
+     * We don't use additional context fields in this step execution,
+     * so we extend SynchronousStepExecution directly and not ArtifactorySynchronousStepExecution
      */
     public static class Execution extends SynchronousStepExecution<GradleBuild> {
         private static final long serialVersionUID = 1L;
