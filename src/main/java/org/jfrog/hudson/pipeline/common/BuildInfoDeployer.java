0624b4c111d6fd361fa0e993c7e36d7fd6ce5e05
Brian Thompson <brian@hashvault.io>
2020-10-27 05:33:21 -0500

    Add import of org.jfrog.build.api.Module to avoid compile type error

--- src/main/java/org/jfrog/hudson/pipeline/common/BuildInfoDeployer.java
+++ src/main/java/org/jfrog/hudson/pipeline/common/BuildInfoDeployer.java
@@ -6,6 +6,7 @@ import jenkins.model.Jenkins;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jfrog.build.api.*;
+import org.jfrog.build.api.Module;
 import org.jfrog.build.api.builder.BuildInfoBuilder;
 import org.jfrog.build.extractor.clientConfiguration.client.ArtifactoryBuildInfoClient;
 import org.jfrog.hudson.AbstractBuildInfoDeployer;
